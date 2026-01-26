6d9fe2225cef4110b627c0b56c94d7747f6a0db8
Fidian Peralta Lopez <donyoh1@hotmail.com>
2026-01-26 14:52:05 -0500

    adding ternary command in file

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -46,3 +46,9 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Is not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);

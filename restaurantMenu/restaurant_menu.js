98f889018ef663b136560ac770262c64cfcaf5eb
Fidian Peralta Lopez <donyoh1@hotmail.com>
2026-02-02 16:05:01 -0500

    Updating items prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,11 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+breakfastMenu.splice( 0, 4, 'Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15');
+console.log(breakfastMenu);
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+mainCourseMenu.splice( 0, 4, 'Steak- $42', 'Pasta -$30.99', 'Burger -$33.99', 'Salmon -$45');
+
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+dessertMenu.splice( 0, 4, 'Cake- $5.50', 'Ice Cream -$10.99', 'Pudding -$15.99', 'Fruit Salad -$23.50');
 
 breakfastMenuItemHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemHTML;
